5df76d9dfb5fbc4948bd72152abe25aa59d93cb3
JJLadouceur <jacob.j.ladouceur@gmail.com>
2019-03-21 15:43:25 -0400

    Deleted Hello World, Added App.vue

--- todo/src/App.vue
+++ todo/src/App.vue
@@ -1,28 +1,41 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <app-header :itemCount="items.length" maxItems="maxItems"></app-header>
+    <app-new-item @itemAdded="newItem"></app-new-item>
+    <app-item-grid :items="items" @itemDeleted="deleteItem"></app-item-grid>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ItemGrid from './components/ItemGrid.vue';
+import NewItem from './components/NewItem.vue';
+import Header from './components/Header.vue';
 
 export default {
-  name: 'app',
+  data: function(){
+    return{
+      items:[],
+      maxItems: 10
+    }
+  },
+  methods:{
+    newItem(item){
+      if(this.items.length >= this.maxItems){
+        return alert('Catch up a bit before adding more ToDos')
+      }
+      this.items.push(item);
+    },
+    deleteItem(index){
+      this.items.splice(index, 1);
+    }
+  },
   components: {
-    HelloWorld
+    appItemGrid: ItemGrid,
+    appNewItem: NewItem,
+    appHeader: Header
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
