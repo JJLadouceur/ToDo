e85b58338e6cf04fe32964a3bbaafb470b80787c
JJLadouceur <jacob.j.ladouceur@gmail.com>
2019-03-21 15:53:43 -0400

    Header and ItemGrid Updated

---
+++ todo/src/components/ItemGrid.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="row">
+    <app-quote v-for="(item, index) in items" @click.native="deleteItem(index)">{{ quote }}</app-quote>
+  </div>
+</template>
+
+<script>
+import Item from "./Item.vue";
+
+export default {
+  props: ["items"],
+  components: {
+    appItem: Item
+  },
+  methods:{
+      deleteItem(index){
+          this.$emit('itemDeleted', index);
+      }
+  }
+}
+</script>
+
+<style>
+</style>
