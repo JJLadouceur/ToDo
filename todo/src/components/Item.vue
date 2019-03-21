3500f637482e1af7a673293c0ed0586fc5045532
JJLadouceur <jacob.j.ladouceur@gmail.com>
2019-03-21 16:14:16 -0400

    Enabled the item to render when added

--- todo/src/components/Item.vue
+++ todo/src/components/Item.vue
@@ -1,7 +1,7 @@
 <template>
    <div class="col-sm-6 col-md-4 col-lg-3">
        <div class="panel panel-default">
-           <div class="panel-body quote">
+           <div class="panel-body item">
                <slot></slot>
            </div>
        </div>
@@ -18,11 +18,11 @@
         color: #141414;
     }
 
-    .quote{
+    .item{
         cursor: pointer;
     }
 
-    .quote:hover{
+    .item:hover{
         background-color: #ffe2e2;
     }
 </style>
