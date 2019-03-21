672636a17e02b8e867ff18fb36217622c02d7fa9
JJLadouceur <jacob.j.ladouceur@gmail.com>
2019-03-21 16:22:55 -0400

    Typo on Header

--- todo/src/components/Header.vue
+++ todo/src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="row">
         <div class="col-sm-12">
-            <h3>Quotes Limit</h3>
+            <h3>Items</h3>
             <div class="progress">
                 <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" :style="{width: (quoteCount / maxQuotes) * 100 + '%'}">
                     {{ itemCount }} / {{ maxItems }}
